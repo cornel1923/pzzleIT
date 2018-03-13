43abb16b9b8388d1f01cf48aea639de0592e5cf7
cornel.gheorghita <cornel.gheorghita@cegeka.com>
2018-03-13 16:44:30 +0200

    Add meterial-ui and change app.js

--- pzzleIT/app/App.js
+++ pzzleIT/app/App.js
@@ -1,9 +1,115 @@
 import React from 'react';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import GridTile from 'material-ui/GridList/GridTile';
+import GridList from 'material-ui/GridList';
 
-const App = props => (
-  <div className="App">
-    {React.Children.only(props.children)}
-  </div>
+const App = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <div>
+      <AppBar title="My AppBar" />
+      <GridList cols={12} cellHeight={200} padding={1} >
+        <GridTile
+          title="Ianuarie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Februarie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Martie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Aprilie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Mai"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Iunie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Iulie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="August"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Septembrie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Octombrie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Noiembrie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+        <GridTile
+          title="Decembrie"
+          cols={4}
+          titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+          rows={2}
+        >
+          <img alt="test" src="http://www.material-ui.com/images/grid-list/00-52-29-429_640.jpg" />
+        </GridTile>
+      </GridList>
+    </div>
+  </MuiThemeProvider>
 );
 
 export default App;
