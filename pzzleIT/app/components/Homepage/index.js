ddb9dcffd3c60c8e800020be2e6228da90cc065f
cornel.gheorghita <cornel.gheorghita@cegeka.com>
2018-03-14 17:39:53 +0200

    Include components in wrappers

--- pzzleIT/app/components/Homepage/index.js
+++ pzzleIT/app/components/Homepage/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import Card from '../base/Card';
+import GridList from 'material-ui/GridList';
+import BuildCard from '../base/Card';
 import './styles.css';
 
-const Homepage = () => (
-  <div className="container">
-    <Card />
+const Homepage = () => {
+  // TODO: Move to constants
+  const days = [{ title: 'Ianuarie', image: 'Ianuarie' }, { title: 'Februarie', image: 'Ianuarie' },
+  { title: 'Martie', image: 'Ianuarie' }, { title: 'Aprilie', image: 'Ianuarie' },
+  { title: 'Mai', image: 'Ianuarie' }, { title: 'Iunie', image: 'Ianuarie' },
+  { title: 'Iulie', image: 'Ianuarie' }, { title: 'August', image: 'Ianuarie' },
+  { title: 'Septembrie', image: 'Ianuarie' }, { title: 'Octombrie', image: 'Ianuarie' },
+  { title: 'Noiembrie', image: 'Ianuarie' }, { title: 'Decembrie', image: 'Ianuarie' }];
+
+  return (<div className="container">
+    <GridList cols={12} cellHeight={200} padding={1} >
+      {days.map(day => BuildCard(day.title, day.image))}
+    </GridList>
   </div>);
+};
 
 export default Homepage;
--- pzzleIT/app/components/base/Card/index.js
+++ pzzleIT/app/components/base/Card/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import GridTile from 'material-ui/GridList/GridTile';
+import v4 from 'uuid';
 import './styles.css';
 
-const Card = () => (<div className="fixed-rectangle" />);
+const BuildCard = title => (<GridTile
+  key={v4()}
+  title={title}
+  cols={4}
+  titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+  rows={2}>
+  <img alt={title} src={'https://image.freepik.com/free-photo/blue-sky-with-clouds_1127-282.jpg'} />
+</GridTile>);
 
-export default Card;
+export default BuildCard;
